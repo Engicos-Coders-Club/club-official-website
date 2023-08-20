a18987f61952b604ab923b6f3e8cae02cd808523
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2023-08-20 14:53:30 +0530

    added comments to code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import Link from 'next/link'
 
+// to open a page without reloading (why react was made in the first place)
+// https://nextjs.org/docs/app/api-reference/components/link
+import Link from 'next/link'  
 export default function Home() {
   return (
     <main className={styles.main}>
