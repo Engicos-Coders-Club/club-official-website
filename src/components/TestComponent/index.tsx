8c0fbfb1e9ec3c73e14b4a1dd04f95e343ccbf43
Shridhar <shridharkamat10@gmail.com>
2023-08-20 20:51:54 +0530

    feat: added components directory

---
+++ src/components/TestComponent/index.tsx
@@ -0,0 +1,10 @@
+//This is just a test component for reference purpose to understand how we are structuring our component
+import React from 'react'
+
+function TestComponent() {
+  return (
+    <div>TestComponent</div>
+  )
+}
+
+export { TestComponent }
