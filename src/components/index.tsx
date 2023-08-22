f674bc2b0a98d5d1fdfac67eb02d595fee4903cf
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2023-08-22 18:23:44 +0530

    bug fix: added braces to export

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,4 @@
-export HomepageProjectCard  from "./HomepageProjectCard/index";
+export { HomepageProjectCard } from "./HomepageProjectCard/index";
 
 export { TestComponent } from "./TestComponent/index";
 
