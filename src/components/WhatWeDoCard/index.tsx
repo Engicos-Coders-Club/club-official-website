b4c72e2419be0feee2bebbc84a3c168343cd405f
Shridhar <shridharkamat10@gmail.com>
2023-08-28 01:41:33 +0530

    feat: added What We Do section

--- src/components/WhatWeDoCard/index.tsx
+++ src/components/WhatWeDoCard/index.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
 import "./styles.css"
 
-function WhatWeDoCard() {
+interface WhatWeDoCardProps {
+  title:string,
+  description:string
+}
+
+function WhatWeDoCard({ title, description }: WhatWeDoCardProps) {
   return (
     <div className='wwd-card'>
-      <p className='wwd-card-title'>Conduct Trainings Etc</p>
-      <p className='wwd-card-description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad</p>
+      <p className='wwd-card-title'>{title}</p>
+      <p className='wwd-card-description'>{description}</p>
     </div>
   )
 }
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,10 +1,6 @@
 export { HomepageProjectCard } from "./HomepageProjectCard";
-
 export { TestComponent } from "./TestComponent";
-
 export { HomepageProjectSection } from "./HomepageProjectSection"
-
 export { EventCard } from "./EventCard";
-
 export { WhatWeDoCard } from "./WhatWeDoCard";
 export { WhatWeDoSection } from "./WhatWeDoSection";
