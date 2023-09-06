4b3f421868d8fac5e2c6fded65319d2041b6b529
Shridhar-dev <shridharkamat10@gmail.com>
2023-09-06 19:20:21 +0530

    feat: page header and events page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import { HomepageProjectSection, EventCard, WhatWeDoSection, Footer } from '../components'
-import Event from "../../public/event.webp"
+import { HomepageProjectSection, EventCard, WhatWeDoSection, Footer, PageHeader } from '../components'
 
 // to open a page without reloading (why react was made in the first place)
 // https://nextjs.org/docs/app/api-reference/components/link
@@ -12,31 +11,9 @@ export default function Home() {
   return (
     <main className={styles.main}>
 
-
-     
+      <PageHeader title={"GEC CODERS' CLUB"} desc={"for, by & of the students."}/>
       <HomepageProjectSection/>
-      <div className='event-grid'>
-        <EventCard 
-              img={Event.src}
-              tags={["🤝 open source", "💻 coding"]}
-              date="28 aug, 2023"
-              title="Roadmap to Software Developement"
-              hosts={["Azvern Dias"]}
-              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad"
-        />
-        <EventCard 
-              img={Event.src}
-              tags={["🤝 open source", "💻 coding"]}
-              date="28 aug, 2023"
-              title="Roadmap to Software Developement"
-              hosts={["Azvern Dias"]}
-              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad"
-        />
-      </div>
+      
       <WhatWeDoSection />
       
     </main>
