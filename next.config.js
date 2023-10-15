e4c533751c9c51cfb38fdb21d1b1007cda64e529
Shridhar <shridharkamat10@gmail.com>
2023-10-16 01:02:46 +0530

    fix: build issues

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export',
+    images: {
+        loader: 'akamai',
+        path: './loaders/akamai.js',
+        unoptimized:true
+    },
+}
 
 export default nextConfig
