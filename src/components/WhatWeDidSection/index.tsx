3bce99a605eafbcf218f7c1f860323d8ce7739b9
tu2-atmanand <sanketgauns8@gmail.com>
2024-01-28 21:31:48 +0530

    Fixed View more link position

--- src/components/WhatWeDidSection/index.tsx
+++ src/components/WhatWeDidSection/index.tsx
@@ -4,12 +4,16 @@ import './style.css';
 const WhatWeDidSection: React.FC = () => {
     return (
         <div className="WhatWeDidSection">
-            <p className='wwdid-section-title'>WHAT <span>WE DID</span></p>
+            <div className='wwb-section-header'>
+                <p className='wwdid-section-title'>WHAT <span>WE DID</span></p>
+                <a href="/events" className="view-more-button">View More <span>-&gt;</span></a>
+            </div>
             <div className="section-content">
                 <WhatWeDidCard heading="GitHub Workshop" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad" />
                 <WhatWeDidCard heading="Basic Concepts of Coding" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad" />
                 <WhatWeDidCard heading="Software Development talk" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad" />
-                <a href="/events" className="view-more-button">View More <span>-&gt;</span></a>
+                <WhatWeDidCard heading="Software Development talk" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad" />
+                <WhatWeDidCard heading="Software Development talk" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad" />
                 {/* Add more WhatWeBuiltCard components as needed */}
             </div>
         </div>
